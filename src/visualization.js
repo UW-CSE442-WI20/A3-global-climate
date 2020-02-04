5c6f62186ee27299545d3b37eb7091d6f3826f26
Lynda <f8122dac91@gmail.com>
2020-02-03 17:46:11 -0800

    Add boilerplate code

---
+++ src/visualization.js
@@ -0,0 +1,123 @@
+(function () {
+  var margin = { top: 10, left: 10, right: 10, bottom: 10 },
+      height = 600 - margin.top - margin.bottom,
+      width  = 960 - margin.left - margin.right;
+
+  // The svg
+  var svg = d3.select("#map-root")
+        .append("svg")
+        .attr("height", height + margin.top + margin.bottom)
+        .attr("width", width + margin.left + margin.right);
+
+  // Map and projection
+  var path = d3.geoPath();
+  var projection = d3.geoNaturalEarth()
+      .scale(width / 2 / Math.PI)
+      .translate([width / 2, height / 2])
+  var path = d3.geoPath()
+      .projection(projection);
+
+  // Data and color scale
+  var data = d3.map();
+  var colorScheme = d3.schemeReds[6];
+  colorScheme.unshift("#eee")
+  var colorScale = d3.scaleThreshold()
+      .domain([1, 6, 11, 26, 101, 1001])
+      .range(colorScheme);
+
+  // Legend
+  var g = svg.append("g")
+      .attr("class", "legendThreshold")
+      .attr("transform", "translate(20,20)");
+  g.append("text")
+      .attr("class", "caption")
+      .attr("x", 0)
+      .attr("y", -6)
+      .text("Legend Title");
+  var labels = ['0', '1-5', '6-10', '11-25', '26-100', '101-1000', '> 1000'];
+  var legend = d3.legendColor()
+      .labels(function (d) { return labels[d.i]; })
+      .shapePadding(4)
+      .scale(colorScale);
+  svg.select(".legendThreshold")
+      .call(legend);
+
+  // Load external data and boot
+  // geojson topology: http://enjalot.github.io/wwsd/data/world/world-110m.geojson
+  d3.queue()
+      .defer(d3.json, "./data/world-110m.geojson")
+      .defer(d3.csv, "./data/countries.csv", function(d) { data.set(d.code, +d.total); })
+      .await(ready);
+
+  function ready(error, topo) {
+      if (error) throw error;
+
+      // Draw the map
+      svg.append("g")
+          .attr("class", "countries")
+          .selectAll("path")
+          .data(topo.features)
+          .enter().append("path")
+              .attr("fill", function (d){
+                  // Pull data for this country
+                  d.total = data.get(d.id) || 0;
+                  // Set the color
+                  return colorScale(d.total);
+              })
+              .attr("d", path);
+  }
+})();
+
+(function () {
+  // set the dimensions and margins of the graph
+  var margin = {top: 50, right: 50, bottom: 50, left: 50},
+      height = 300 - margin.top - margin.bottom,
+      width  = 960 - margin.left - margin.right;
+
+  // append the svg object to the body of the page
+  var svg = d3.select("#chart-root")
+    .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+
+  //Read the data
+  d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/3_TwoNumOrdered_comma.csv",
+
+    // When reading the csv, I must format variables:
+    function(d){
+      return { date : d3.timeParse("%Y-%m-%d")(d.date), value : d.value }
+    },
+
+    // Now I can use this dataset:
+    function(data) {
+
+      // Add X axis --> it is a date format
+      var x = d3.scaleTime()
+        .domain(d3.extent(data, function(d) { return d.date; }))
+        .range([ 0, width ]);
+      svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+
+      // Add Y axis
+      var y = d3.scaleLinear()
+        .domain([0, d3.max(data, function(d) { return +d.value; })])
+        .range([ height, 0 ]);
+      svg.append("g")
+        .call(d3.axisLeft(y));
+
+      // Add the line
+      svg.append("path")
+        .datum(data)
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-width", 1.5)
+        .attr("d", d3.line()
+          .x(function(d) { return x(d.date) })
+          .y(function(d) { return y(d.value) })
+      )
+    });
+})();
