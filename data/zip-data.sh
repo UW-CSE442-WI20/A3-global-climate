3c203a86e47b740cc9b89ee31c0ba514c87b90a5
Jaclyn Brockschmidt <jcbrockschmidt@gmail.com>
2020-02-04 01:50:43 -0800

    Add script for merging temperature data for all countries

--- data/zip-data.sh
+++ data/zip-data.sh
@@ -4,9 +4,17 @@
 
 PREP_DIR="./prepared"
 
+# Zip data for individual countries.
 for CTG in "TAVG" "TMAX" "TMIN"
 do
     OUT="$CTG-by-country.zip"
     echo "Zipping $CTG data as $OUT..."
     zip "$OUT" "$PREP_DIR/"*"-$CTG.csv"
 done
+
+# Zip data merging countries.
+NUM_ALL=$(ls -1 "$PREP_DIR/"*"-all.csv" | wc -l)
+if [ "$NUM_ALL" -gt 0 ]
+then
+    zip "TEMP-all.zip" "$PREP_DIR/"*"-all.csv"
+fi
