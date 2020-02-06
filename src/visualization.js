b829601f94d73ca9ae4d3ac07b74de2df500906b
zaynaharif <zaynaharif@gmail.com>
2020-02-05 17:19:48 -0800

    Country dropdown menu and slider

--- src/visualization.js
+++ src/visualization.js
@@ -123,3 +123,34 @@
       )
     });
 })();
+
+(function () {
+    var dataTime = d3.range(0, 10).map(function(d) {
+        return new Date(1995 + d, 10, 3);
+    });
+
+    var sliderTime = d3
+        .sliderBottom()
+        .min(d3.min(dataTime))
+        .max(d3.max(dataTime))
+        .step(1000 * 60 * 60 * 24 * 365)
+        .width(875)
+        .tickFormat(d3.timeFormat('%Y'))
+        .tickValues(dataTime)
+        .default(new Date(1998, 10, 3))
+    // .on('onchange', val => {
+    //   d3.select('p#value-time').text(d3.timeFormat('%Y')(val));
+    // });
+
+    var gTime = d3
+        .select('div#slider-time')
+        .append('svg')
+        .attr('width', 1000)
+        .attr('height', 100)
+        .append('g')
+        .attr('transform', 'translate(30,30)');
+
+    gTime.call(sliderTime);
+
+    // d3.select('p#value-time').text(d3.timeFormat('%Y')(sliderTime.value()));
+}) ();
